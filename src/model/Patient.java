00105c7dd288b2d0c0ba347192579e9e6ba49d53
MSM Shahla <maryamshahla465@gmail.com>
2026-03-13 12:07:52 +0530

    Added patient class extending Person

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,4 +1,37 @@
 package model;
 
-public class Patient {
+public class Patient extends Person{
+    private int patientId;
+    private String medicalHistory;
+
+    public Patient(String name, int age, String phoneNumber, String medicalHistory, int id) {
+        super(name, age, phoneNumber);
+        this.medicalHistory = medicalHistory;
+        this.patientId = id;
+    }
+
+    public int getId() {
+        return patientId;
+    }
+
+    public void setId(int id) {
+        this.patientId = id;
+    }
+
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId +
+               "Name: " + getName() +
+               "Age: " + getAge() +
+               "Phone: " + getPhoneNumber() +
+               "Medical History: " + getMedicalHistory();
+    }
 }
