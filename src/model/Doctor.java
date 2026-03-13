aa93f1dfe1ba7b452e0ea7ea29d7ba0042a75a4a
MSM Shahla <maryamshahla465@gmail.com>
2026-03-13 12:15:14 +0530

    Add Doctor class extending Person

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,4 +1,37 @@
 package model;
 
-public class Doctor {
+public class Doctor extends Person {
+    private int doctorId;
+    private String specialization;
+
+    public Doctor(String name, int age, String phoneNumber, int doctorId, String specialization) {
+        super(name, age, phoneNumber);
+        this.doctorId = doctorId;
+        this.specialization = specialization;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor ID: " + doctorId +
+                "Name: " + getName() +
+                "Age: " + getAge() +
+                "Phone: " + getPhoneNumber() +
+                "Specialization: " + specialization;
+    }
 }
